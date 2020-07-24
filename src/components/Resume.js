e77cb161deacdd35f683ff3264aee5686bd87569
Ravi Varshney <ravivarshney49@gmail.com>
2020-07-24 10:26:30 +0530

    added fontawesome and personal details

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,15 +2,30 @@ import React, { Component } from "react"
 import PersonalDetails from "./PersonalDetails"
 
 class Resume extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      details: {
+        name: "John Doe",
+        twitter: "johnD",
+        linkedIn: "johnD",
+        github: "johnD",
+        email: "john@john.com",
+        mobile: "+1-12345-6622",
+      },
+    }
+  }
+
   render() {
     return (
       <div
         style={{
-          size: `7in 9.25in`,
+          height: `297mm`,
           padding: `10mm 16mm 27mm 10mm`,
+          margin: 0,
         }}
       >
-        <PersonalDetails />
+        <PersonalDetails details={this.state.details} />
       </div>
     )
   }
