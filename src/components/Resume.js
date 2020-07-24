1847d2fa943d81e984060344e43b6b6b7fa6093d
Ravi Varshney <ravivarshney49@gmail.com>
2020-07-24 11:51:29 +0530

    added experience slot

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import PersonalDetails from "./PersonalDetails"
+import Experience from "./Experience"
 
 class Resume extends Component {
   constructor(props) {
@@ -13,6 +14,32 @@ class Resume extends Component {
         email: "john@john.com",
         mobile: "+1-12345-6622",
       },
+      exp: [
+        {
+          role: "SDE",
+          company: "Facebook",
+          from: "July 19",
+          to: "Present",
+          location: "Remote",
+          points: [
+            "made awesome react",
+            "made redux ",
+            "working with react team",
+          ],
+        },
+        {
+          role: "SDE",
+          company: "Facebook",
+          from: "July 19",
+          to: "Present",
+          location: "Remote",
+          points: [
+            "made awesome react",
+            "made redux ",
+            "working with react team",
+          ],
+        },
+      ],
     }
   }
 
@@ -29,9 +56,12 @@ class Resume extends Component {
         <div className="flex pt-6">
           <div className="w-7/12">
             {/* left side */}
-            <h2 className="w-full text-2xl text-blue-600 border-b-2 border-blue-600 font-medium">
+            <h2 className="w-full text-2xl text-blue-600 border-b-2 border-blue-600 font-medium mb-3">
               EXPERIENCE
             </h2>
+            {this.state.exp.map((ex, i) => (
+              <Experience ex={ex} key={i} />
+            ))}
           </div>
           <div className="w-1/12">{/* empty space */}</div>
           <div className="w-4/12">
